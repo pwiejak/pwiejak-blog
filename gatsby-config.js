4e20388e483a1d8fc742affd9b7994465c800da2
pwiejak <pwiejak@future-processing.com>
2022-02-10 09:22:59 +0100

    cleanup and profile picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Development allrounder blog`,
+    title: `Programming and all things around`,
     author: {
       name: `Paweł Wiejak`,
       summary: `.NET developer, sports enthusiast, fantasy fan.`,
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `opalpw1`,
     },
   },
   plugins: [
