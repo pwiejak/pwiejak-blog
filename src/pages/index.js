74388f58a1b2ed04b5b7dbbbc07d5ba2e0b74182
pwiejak <pwiejak@future-processing.com>
2022-02-19 20:11:38 +0100

    dark theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ const BlogIndex = ({ data, location }) => {
           const title = post.frontmatter.title || post.fields.slug
 
           return (
-            <li key={post.fields.slug}>
+            <li key={post.fields.slug} className="synopsis">
               <article
                 className="post-list-item"
                 itemScope
